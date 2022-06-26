42c46a5e517bec708081e21f659c47d655771793
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-06-26 20:02:49 -0300

    ref(veiculo): sizeof instead of harcoded again

--- veiculo.c
+++ veiculo.c
@@ -27,11 +27,11 @@ veiculo ler_veiculo(FILE *stream, int tamRegistro){
     v.modelo = NULL;
 
     // campos de tamanho fixo
-    fread(&(v.id), 4, 1, stream);
-    fread(&(v.ano), 4, 1, stream);
-    fread(&(v.qtt), 4, 1, stream);
-    fread(v.sigla, 1, 2, stream);
-    int tamAtual = 4 + 4 + 4 + 2;
+    fread(&(v.id), sizeof(int), 1, stream);
+    fread(&(v.ano), sizeof(int), 1, stream);
+    fread(&(v.qtt), sizeof(int), 1, stream);
+    fread(v.sigla, sizeof(char), 2, stream);
+    int tamAtual = sizeof(int) + sizeof(int) + sizeof(int) + (sizeof(char) * 2);
 
     // campos de tamanho variavel
     char c;
@@ -40,25 +40,25 @@ veiculo ler_veiculo(FILE *stream, int tamRegistro){
 
         int tam;
         char codigo;
-        fread(&tam, 4, 1, stream);
-        fread(&codigo, 1, 1, stream);
+        fread(&tam, sizeof(int), 1, stream);
+        fread(&codigo, sizeof(char), 1, stream);
 
         if (codigo == '0'){
             v.cidade = malloc((tam + 1) * sizeof(char));
-            fread(v.cidade, 1, tam, stream);
+            fread(v.cidade, sizeof(char), tam, stream);
             v.cidade[tam] = '\0';
         }
         else if (codigo == '1'){
             v.marca = malloc((tam + 1) * sizeof(char));
-            fread(v.marca, 1, tam, stream);
+            fread(v.marca, sizeof(char), tam, stream);
             v.marca[tam] = '\0';
         }
         else if (codigo == '2'){
             v.modelo = malloc((tam + 1) * sizeof(char));
-            fread(v.modelo, 1, tam, stream);
+            fread(v.modelo, sizeof(char), tam, stream);
             v.modelo[tam] = '\0';
         }
-        tamAtual += 4 + 1 + tam;
+        tamAtual += sizeof(int) + sizeof(char) + tam;
     }
     ungetc(c, stream);
 
@@ -70,43 +70,43 @@ veiculo ler_veiculo(FILE *stream, int tamRegistro){
 
 void escrever_veiculo(veiculo v, FILE *stream){
     // campos de tamanho fixo
-    fwrite(&v.id, 4, 1, stream);
-    fwrite(&v.ano, 4, 1, stream);
-    fwrite(&v.qtt, 4, 1, stream);
-    fwrite(v.sigla, 1, 2, stream);
+    fwrite(&v.id, sizeof(int), 1, stream);
+    fwrite(&v.ano, sizeof(int), 1, stream);
+    fwrite(&v.qtt, sizeof(int), 1, stream);
+    fwrite(v.sigla, sizeof(char), 2, stream);
 
     // campos de tamanho variavel
     if (v.cidade){
         int tamCidade = strlen(v.cidade);
         char codigo = '0';
-        fwrite(&tamCidade, 4, 1, stream);
-        fwrite(&codigo, 1, 1, stream);
-        fwrite(v.cidade, 1, tamCidade, stream);
+        fwrite(&tamCidade, sizeof(int), 1, stream);
+        fwrite(&codigo, sizeof(char), 1, stream);
+        fwrite(v.cidade, sizeof(char), tamCidade, stream);
     }
     if (v.marca){
         int tamMarca = strlen(v.marca);
         char codigo = '1';
-        fwrite(&tamMarca, 4, 1, stream);
-        fwrite(&codigo, 1, 1, stream);
-        fwrite(v.marca, 1, tamMarca, stream);
+        fwrite(&tamMarca, sizeof(int), 1, stream);
+        fwrite(&codigo, sizeof(char), 1, stream);
+        fwrite(v.marca, sizeof(char), tamMarca, stream);
     }
     if (v.modelo){
         int tamModelo = strlen(v.modelo);
         char codigo = '2';
-        fwrite(&tamModelo, 4, 1, stream);
-        fwrite(&codigo, 1, 1, stream);
-        fwrite(v.modelo, 1, tamModelo, stream);
+        fwrite(&tamModelo, sizeof(int), 1, stream);
+        fwrite(&codigo, sizeof(char), 1, stream);
+        fwrite(v.modelo, sizeof(char), tamModelo, stream);
     }
 }
 
 int calcular_tamanho(veiculo v){
-    int tamanho = 4 + 4 + 4 + 2;
+    int tamanho = sizeof(int) + sizeof(int) + sizeof(int) + (sizeof(char) * 2);
     if (v.cidade)
-        tamanho += 1 + 4 + strlen(v.cidade);
+        tamanho += sizeof(char) + sizeof(int) + strlen(v.cidade);
     if (v.marca)
-        tamanho += 1 + 4 + strlen(v.marca);
+        tamanho += sizeof(char) + sizeof(int) + strlen(v.marca);
     if (v.modelo)
-        tamanho += 1 + 4 + strlen(v.modelo);
+        tamanho += sizeof(char) + sizeof(int) + strlen(v.modelo);
     return tamanho;
 }
 
@@ -161,7 +161,7 @@ long int filtrarVeiculo(FILE *stream, veiculo f, char tipo, veiculo *v, long int
 
     // campos de tamanho fixo
     char removido;
-    fread(&removido, 1, 1, stream);
+    fread(&removido, sizeof(char), 1, stream);
     lido++;
     if(tipo == '1'){
         // para o tipo1 os registros tem sempre 97 bytes
@@ -172,48 +172,48 @@ long int filtrarVeiculo(FILE *stream, veiculo f, char tipo, veiculo *v, long int
         }
         // ignora o campo de proximo arquivo removido ja que nao lidaremos com
         // arquivos removidos
-        fseek(stream, 4, SEEK_CUR);
-        lido += 4;
+        fseek(stream, sizeof(int), SEEK_CUR);
+        lido += sizeof(int);
     }
     else if(tipo == '2'){
         // para o tipo2 o tamanho total esta armazenado no inicio do registro
-        fread(&tamRegistro, 4, 1, stream);
-        lido += 4;
+        fread(&tamRegistro, sizeof(int), 1, stream);
+        lido += sizeof(int);
         // o tamanho armazenado no inicio do registro nao considera os 4 bytes
         // do proprio campo nem o byte do campo de identificador de registros
         // removido mas o funcionamento dessa funcao requer que sejam levados
         // em consideracao todos os bytes do registro portanto somamos 5
-        tamRegistro += 5;
+        tamRegistro += sizeof(int) + sizeof(char);
         if(removido == '1'){
             *next = tamRegistro - lido;
             return -1;
         }
         // para o tipo2 o campo de proximo arquivo removido e um long int
-        fseek(stream, 8, SEEK_CUR);
-        lido += 8;
+        fseek(stream, sizeof(long), SEEK_CUR);
+        lido += sizeof(long);
     }
-    fread(&v->id, 4, 1, stream);
-    lido += 4;
+    fread(&v->id, sizeof(int), 1, stream);
+    lido += sizeof(int);
     // se o filtro exigir o campo id compara o id do registro com o do filtro
     if((f.id != -1) && (v->id != f.id)){
         *next = tamRegistro - lido;
         return -1;
     }
-    fread(&v->ano, 4, 1, stream);
-    lido += 4;
+    fread(&v->ano, sizeof(int), 1, stream);
+    lido += sizeof(int);
     // se o filtro exigir o campo ano compara o ano do registro com o do filtro
     if((f.ano != -1) && (v->ano != f.ano)){
         *next = tamRegistro - lido;
         return -1;
     }
-    fread(&v->qtt, 4, 1, stream);
-    lido += 4;
+    fread(&v->qtt, sizeof(int), 1, stream);
+    lido += sizeof(int);
     // se o filtro exigir o campo qtt compara o qtt do registro com o do filtro
     if((f.qtt != -1) && (v->qtt != f.qtt)){
         *next = tamRegistro - lido;
         return -1;
     }
-    lido += fread(v->sigla, 1, 2, stream);
+    lido += fread(v->sigla, sizeof(char), 2, stream);
     // se o filtro exigir o campo sigla compara a do registro com a do filtro
     if(strncmp(f.sigla, "$$", 2) && strncmp(v->sigla, f.sigla, 2)){
         *next = tamRegistro - lido;
@@ -244,15 +244,15 @@ long int filtrarVeiculo(FILE *stream, veiculo f, char tipo, veiculo *v, long int
         codigo = -1;
     }
     else{
-        fread(&tam, 4, 1, stream);
-        lido += 4;
-        lido += fread(&codigo, 1, 1, stream);
+        fread(&tam, sizeof(int), 1, stream);
+        lido += sizeof(int);
+        lido += fread(&codigo, sizeof(char), 1, stream);
     }
     // codigo 0 se refere ao campo cidade
     if(codigo == '0'){
         // aloca 1 byte a mais que o tamanho informado para o \0
-        v->cidade = malloc(tam + 1);
-        lido += fread(v->cidade, 1, tam, stream);
+        v->cidade = malloc((tam + 1) * sizeof(char));
+        lido += fread(v->cidade, sizeof(char), tam, stream);
         v->cidade[tam] = '\0';
         // se o filtro exigir o campo cidade compara com o do registro
         // se tiver chegado ao final do registro checa se o filtro exige algum
@@ -271,9 +271,9 @@ long int filtrarVeiculo(FILE *stream, veiculo f, char tipo, veiculo *v, long int
             codigo = -1;
         }
         else{
-            fread(&tam, 4, 1, stream);
-            lido += 4;
-            lido += fread(&codigo, 1, 1, stream);
+            fread(&tam, sizeof(int), 1, stream);
+            lido += sizeof(int);
+            lido += fread(&codigo, sizeof(char), 1, stream);
         }
     }
     else{
@@ -285,8 +285,8 @@ long int filtrarVeiculo(FILE *stream, veiculo f, char tipo, veiculo *v, long int
     }
     // codigo 1 se refere ao campo marca
     if(codigo == '1'){                                                                                                     
-        v->marca = malloc(tam + 1);
-        lido += fread(v->marca, 1, tam, stream);
+        v->marca = malloc((tam + 1) * sizeof(char));
+        lido += fread(v->marca, sizeof(char), tam, stream);
         v->marca[tam] = '\0';
         if((f.marca && strcmp(v->marca, f.marca)) || ((lido >= tamRegistro) && f.modelo)){
             *next = tamRegistro - lido;
@@ -301,9 +301,9 @@ long int filtrarVeiculo(FILE *stream, veiculo f, char tipo, veiculo *v, long int
             codigo = -1;
         }
         else{
-            fread(&tam, 4, 1, stream);
-            lido += 4;
-            lido += fread(&codigo, 1, 1, stream);
+            fread(&tam, sizeof(int), 1, stream);
+            lido += sizeof(int);
+            lido += fread(&codigo, sizeof(char), 1, stream);
         }
     }
     else{
@@ -314,8 +314,8 @@ long int filtrarVeiculo(FILE *stream, veiculo f, char tipo, veiculo *v, long int
     }
     // codigo 2 se refere ao campo modelo
     if(codigo == '2'){
-        v->modelo = malloc(tam + 1);
-        lido += fread(v->modelo, 1, tam, stream);
+        v->modelo = malloc((tam + 1) * sizeof(char));
+        lido += fread(v->modelo, sizeof(char), tam, stream);
         v->modelo[tam] = '\0';
         if(f.modelo && strcmp(v->modelo, f.modelo)){
             *next = tamRegistro - lido;
@@ -420,7 +420,7 @@ void atualizar_veiculo_1(veiculo *v, veiculo *valores, veiculo *campos){
             v->cidade = NULL;
         }
         if(valores->cidade){
-            v->cidade = realloc(v->cidade, (strlen(valores->cidade) * sizeof(char)) + 1);
+            v->cidade = realloc(v->cidade, (strlen(valores->cidade) + 1) * sizeof(char));
             // se o campo deve ser atualizado e se foi dado um novo valor para ele copia o valor para o objeto
             strcpy(v->cidade, valores->cidade);
         }
@@ -431,7 +431,7 @@ void atualizar_veiculo_1(veiculo *v, veiculo *valores, veiculo *campos){
             v->marca = NULL;
         }
         if(valores->marca){
-            v->marca = realloc(v->marca, (strlen(valores->marca) * sizeof(char)) + 1);
+            v->marca = realloc(v->marca, (strlen(valores->marca) + 1) * sizeof(char));
             strcpy(v->marca, valores->marca);
         }
     }
@@ -441,7 +441,7 @@ void atualizar_veiculo_1(veiculo *v, veiculo *valores, veiculo *campos){
             v->modelo = NULL;
         }
         if(valores->modelo){
-            v->modelo = realloc(v->modelo, (strlen(valores->modelo) * sizeof(char)) + 1);
+            v->modelo = realloc(v->modelo, (strlen(valores->modelo) + 1) * sizeof(char));
             strcpy(v->modelo, valores->modelo);
         }
     }
@@ -451,16 +451,16 @@ void remover_veiculo(FILE *bin, long cur, char tipo, void *rc){
     fseek(bin, cur, SEEK_SET);
     char rem = '1';
     // define o registro como logicamente removido
-    fwrite(&rem, 1, 1, bin);
+    fwrite(&rem, sizeof(char), 1, bin);
     if(tipo == '1'){
         // se for tipo 1 escreve o RRN no topo da pilha no campo de proximo RRN deletado do registro
-        fwrite(&((cabecalho *)rc)->topo1, 4, 1, bin);
+        fwrite(&((cabecalho *)rc)->topo1, sizeof(int), 1, bin);
         // substitui o topo da pilha pelo RRN do ultimo registro
         ((cabecalho *)rc)->topo1 = (cur - 182) / 97;
     }
     else if(tipo == '2'){
         int tam;
-        fread(&tam, 4, 1, bin);
+        fread(&tam, sizeof(int), 1, bin);
         // salva o offset do campo de proximo offset removido
         long prox = ftell(bin);
         long ant = -1;
@@ -468,27 +468,27 @@ void remover_veiculo(FILE *bin, long cur, char tipo, void *rc){
         if(((cabecalho *)rc)->topo2 != -1){
             fseek(bin, ((cabecalho *)rc)->topo2 + 1, SEEK_SET);
             int tam2;
-            fread(&tam2, 4, 1, bin);
+            fread(&tam2, sizeof(int), 1, bin);
             // se o topo da lista ordenada nao estiver vazio procura o proximo registro deletado menor que o ultimo
             while(tam < tam2){
                 ant = ftell(bin);
-                fread(&offset, 8, 1, bin);
+                fread(&offset, sizeof(long), 1, bin);
                 fseek(bin, offset + 1, SEEK_SET);
-                fread(&tam2, 4, 1, bin);
+                fread(&tam2, sizeof(int), 1, bin);
             }
         }
         if(ant == -1){
             // subtitui apenas o topo
             fseek(bin, prox, SEEK_SET);
-            fwrite(&((cabecalho *)rc)->topo2, 8, 1, bin);
+            fwrite(&((cabecalho *)rc)->topo2, sizeof(long), 1, bin);
             ((cabecalho *)rc)->topo2 = cur;
         }
         else{
             // refaz a ligacao dos registros removidos para incluir o novo registro no meio
             fseek(bin, ant, SEEK_SET);
-            fwrite(&cur, 8, 1, bin);
+            fwrite(&cur, sizeof(long), 1, bin);
             fseek(bin, prox, SEEK_SET);
-            fwrite(&offset, 8, 1, bin);
+            fwrite(&offset, sizeof(long), 1, bin);
         }
     }
     ((cabecalho *)rc)->nroRegRem++;
@@ -502,7 +502,7 @@ void inserir_veiculo(void *rc, FILE *bin, veiculo v){
         fseek(bin, ((cabecalho *)rc)->topo2 + 1, SEEK_SET);
         fread(&tamTopo, sizeof(int), 1, bin);
     }
-    int tamRegistro = 8 + calcular_tamanho(v);
+    int tamRegistro = sizeof(long) + calcular_tamanho(v);
     // nao existem registros removidos ou espaco insuficiente -> escrever no fim do arquivo
     if (((cabecalho *)rc)->topo2 == -1 || tamRegistro > tamTopo){
         fseek(bin, ((cabecalho *)rc)->proxByteOffset, SEEK_SET);
@@ -510,7 +510,7 @@ void inserir_veiculo(void *rc, FILE *bin, veiculo v){
         fwrite(&tamRegistro, sizeof(int), 1, bin);
         fwrite(&prox, sizeof(long), 1, bin);
         escrever_veiculo(v, bin);
-        ((cabecalho *)rc)->proxByteOffset += 1 + 4 + tamRegistro;
+        ((cabecalho *)rc)->proxByteOffset += sizeof(char) + sizeof(int) + tamRegistro;
     }
     // existe registro removido com espaco suficiente -> escrever no byteOffset
     else{
