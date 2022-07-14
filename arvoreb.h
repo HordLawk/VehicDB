9a32573f1de95441e3c4c5aef8233fa98c18a98c
HordLawk <22248342+HordLawk@users.noreply.github.com>
2022-07-14 12:01:02 -0300

    feat(btree): insert new index attempt

--- arvoreb.h
+++ arvoreb.h
@@ -1,3 +1,6 @@
+#ifndef _ARVOREB_H
+#define _ARVOREB_H
+#include "indice.h"
 #define ORDEM 4
 #define TAM_ARVB1 45
 #define TAM_ARVB2 57
@@ -7,8 +10,8 @@
 typedef struct Indice_b{
     char tipo;
     int nro_chaves;
-    Indice chaves[ORDEM-1];
-    int desc[ORDEM]; 
+    Indice chaves[ORDEM];
+    int desc[ORDEM + 1]; 
 } Indice_b;
 
 typedef struct Cabecalho_b{
@@ -20,3 +23,8 @@ Indice_b ler_indice_b(FILE *stream, char tipo);
 void escrever_indice_b(FILE *stream, Indice_b ind, char tipo);
 int buscar_indice_b1(FILE *stream, Indice_b ind, int id);
 long buscar_indice_b2(FILE *stream, Indice_b ind, int id);
+void inserir_indice_b(FILE *stream, Indice ind, char tipo);
+Indice_b *alg_insercao_b(FILE *stream, Indice_b *no, Indice ind, char tipo, Cabecalho_b *cabecalho);
+void escrever_cabecalho_b(Cabecalho_b c, FILE *stream, char tipo);
+Cabecalho_b ler_cabecalho_b(FILE *stream, char tipo);
+#endif
